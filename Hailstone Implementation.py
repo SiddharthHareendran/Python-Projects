2cf5e08b0c8b498288ba7515a02330709965cddc
SiddharthHareendran <55367893+SiddharthHareendran@users.noreply.github.com>
2020-02-02 12:23:47 -0600

    Add files via upload
    Hailstone numbers:
    N = N / 2 : For Even N
    N = 3 * (N-1) + 1 : For Odd N
    For a given starting value 𝑥0, the hailstone number is how many steps it takes for the above sequence to first reach the number 1.

---
+++ Hailstone Implementation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def hailstorm(x):
+    count=0
+    while x!=1:
+        if x%2==0:
+            x = x//2
+            count+=1          
+        else:
+            x=(3*x)+1
+            count+=1
+    print(count)
+for i in range(2,100):
+    hailstorm(i)
+get_ipython().run_line_magic('timeit', 'hailstorm')
+
